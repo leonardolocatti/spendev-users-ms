8dcd695f5cc0abe52f697fc671705303a7d7f794
Leonardo Locatti <locatti.leonardo@gmail.com>
2021-12-05 00:03:07 -0300

    spring project created with sonar and jacoco

---
+++ src/test/java/br/com/llocatti/spendev/discovery/UsersApplicationTests.java
@@ -0,0 +1,15 @@
+package br.com.llocatti.spendev.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+  @Test
+  void contextLoads() {
+    assertDoesNotThrow(() -> UsersApplication.main(new String[] {}));
+  }
+}
